e4ccdb0205e6dded930409dbd596ce4b4a96de0b
worawitboat <worawit.pa.61@ubu.ac.th>
2020-09-02 12:10:06 +0700

    Updates

---
+++ precache-manifest.4f60023de5ce3931ffafc872df1c69f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19f94cf75884208bddb4d1ec541870e",
+    "url": "/commath-pj/index.html"
+  },
+  {
+    "revision": "bcafa57b97429b81848f",
+    "url": "/commath-pj/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df804eab6bda7c3e89a5",
+    "url": "/commath-pj/static/js/2.0938f15a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath-pj/static/js/2.0938f15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcafa57b97429b81848f",
+    "url": "/commath-pj/static/js/main.f2dcb501.chunk.js"
+  },
+  {
+    "revision": "e595a991d3f5f102f898",
+    "url": "/commath-pj/static/js/runtime-main.757f409c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commath-pj/static/media/logo.5d5d9eef.svg"
+  }
+]);
