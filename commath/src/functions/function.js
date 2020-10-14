d7529c0fc81f422cd703dae3cc1d862c536b4648
worawitboat <worawit.pa.61@ubu.ac.th>
2020-10-14 11:27:05 +0700

    change domain

--- commath/src/functions/function.js
+++ commath/src/functions/function.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const apiHost = "http://127.0.0.1:8000";
-const apiHost = "https://change-it.cf";
+const apiHost = "https://worawitboat.tk";
 
 export const getB2s = (b2s) => {
     return axios
